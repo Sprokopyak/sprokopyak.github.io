21aa18f69da6adf76ba54b4f0195632df49ebf5c
Stepan Prokopiak <sprokopyak96@gmail.com>
2018-01-11 21:59:58 +0200

    Update email.php

--- smartwatch/email.php
+++ smartwatch/email.php
@@ -1,21 +1,9 @@
-
-
 <?php
-if( isset($_POST['n']) && isset($_POST['e']) && isset($_POST['m']) ){
-	$n = $_POST['n']; // HINT: use preg_replace() to filter the data
-	$e = $_POST['e'];
-	$m = nl2br($_POST['m']);
-	$to = "sprokopyak96@gmail.com";	
-	$from = $e;
-	$subject = 'Contact Form Message';
-	$message = '<b>Name:</b> '.$n.' <br><b>Email:</b> '.$e.' <p>'.$m.'</p>';
-	$headers = "From: $from\n";
-	$headers .= "MIME-Version: 1.0\n";
-	$headers .= "Content-type: text/html; charset=iso-8859-1\n";
-	if( mail($to, $subject, $message, $headers) ){
-		echo "success";
-	} else {
-		echo "The server failed to send the message. Please try again later.";
-	}
-}
+    $toEmail = "admin@phppot_samples.com";
+    $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+    if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+        print "<p class='success'>Mail Sent.</p>";
+    } else {
+        print "<p class='Error'>Problem in Sending Mail.</p>";
+    }
 ?>
